a561c44308052bff5b091ce944dd45579ec57211
Rifat <rifat@setuptech.international>
2019-11-06 14:04:11 +0600

    1. Upgrade library versions, 2. Fix splash screen

--- app/src/main/java/io/diaryofrifat/code/examroutine/ui/splash/SplashActivity.kt
+++ app/src/main/java/io/diaryofrifat/code/examroutine/ui/splash/SplashActivity.kt
@@ -3,16 +3,17 @@ package io.diaryofrifat.code.examroutine.ui.splash
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import io.diaryofrifat.code.examroutine.R
 import io.diaryofrifat.code.examroutine.ui.decisionmaker.DecisionMakerActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-
-        startActivity(Intent(this, DecisionMakerActivity::class.java))
-        finish()
+        startActivity(
+                Intent(this, DecisionMakerActivity::class.java).apply {
+                    this.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    this.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }
+        )
     }
 }
